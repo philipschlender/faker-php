91cf3e9bb3e5a4286a06c87e14c4b855c40f53cd
Philip Schlender <philip_schlender@yahoo.de>
2025-07-19 11:54:12 +0200

    Add exceptions to doc blocks of interfaces

--- src/Generators/CoreInterface.php
+++ src/Generators/CoreInterface.php
@@ -2,14 +2,25 @@
 
 namespace Faker\Generators;
 
+use Faker\Exceptions\FakerException;
+
 interface CoreInterface
 {
     public function randomBoolean(): bool;
 
+    /**
+     * @throws FakerException
+     */
     public function randomFloat(float $minimum = 0.0, float $maximum = PHP_FLOAT_MAX, int $precision = 2): float;
 
+    /**
+     * @throws FakerException
+     */
     public function randomInteger(int $minimum = 0, int $maximum = PHP_INT_MAX): int;
 
+    /**
+     * @throws FakerException
+     */
     public function randomString(int $length = 32): string;
 
     /**
--- src/Generators/FsInterface.php
+++ src/Generators/FsInterface.php
@@ -2,10 +2,18 @@
 
 namespace Faker\Generators;
 
+use Faker\Exceptions\FakerException;
+
 interface FsInterface
 {
+    /**
+     * @throws FakerException
+     */
     public function randomDirectory(int $depth = 1, bool $absolutePath = false): string;
 
+    /**
+     * @throws FakerException
+     */
     public function randomFile(int $depth = 0, bool $absolutePath = false): string;
 
     public function randomExtension(): string;
